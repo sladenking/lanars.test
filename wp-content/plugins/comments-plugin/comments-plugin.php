60ceb78bb551ef2662bdb1fe41d963301050586b
sladenking <deniskosv@gmail.com>
2022-05-20 21:09:40 +0300

    created comments plugin

---
+++ wp-content/plugins/comments-plugin/comments-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Comments Plugin
+ * Plugin URI: http://lanars.test/comments-plugin
+ *
+ * Description: Comments Plugin that serves a shortcode for displaying the post with the most comments - the title,
+ * short description, picture, publication date and the actual number of comments must be displayed.
+ *
+ * Version: 1.0
+ *
+ * Author: Viacheslav Denysko
+ * Author URI: https://www.linkedin.com/in/viacheslav-denysko/
+ */
