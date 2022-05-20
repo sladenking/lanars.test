24281ded12a0ff2682221d68f6d6f396d320211a
sladenking <deniskosv@gmail.com>
2022-05-20 21:22:47 +0300

    created home page; changed header & footer

--- wp-content/themes/lanars-test/footer.php
+++ wp-content/themes/lanars-test/footer.php
@@ -11,22 +11,12 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lanars-test' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'lanars-test' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lanars-test' ), 'lanars-test', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<footer class="footer">
+        <div class="container">
+            site footer
+        </div>
+	</footer>
+</div>
 
 <?php wp_footer(); ?>
 
--- wp-content/themes/lanars-test/header.php
+++ wp-content/themes/lanars-test/header.php
@@ -22,38 +22,23 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'lanars-test' ); ?></a>
+<header class="header">
+    <div class="header-branding">
+        LANARS Test
+    </div>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$lanars_test_description = get_bloginfo( 'description', 'display' );
-			if ( $lanars_test_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $lanars_test_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <nav class="header-nav main-navigation">
+        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'lanars-test' ); ?></button>
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'menu-1',
+                'menu_id'        => 'primary-menu',
+            )
+        );
+        ?>
+    </nav>
+</header>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'lanars-test' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+
+
